45c2b38418016bf6362c1411bba087ff986683f7
Mateusz Bednarski <echelon133@gmail.com>
2017-05-28 18:32:52 +0200

    Added __str__ representation for current models

--- blog_app/models.py
+++ blog_app/models.py
@@ -9,6 +9,9 @@ def generate_id():
 class Category(models.Model):
     name = models.CharField(max_length=40)
 
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     article_id = models.CharField(primary_key=True, 
@@ -22,3 +25,6 @@ class Article(models.Model):
     category = models.ManyToManyField(Category)
     article_body = models.TextField()
     slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
