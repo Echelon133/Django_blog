48350f7c390aa1fdc7df3fef752c01d7f291cc35
Mateusz Bednarski <echelon133@gmail.com>
2017-05-30 14:51:31 +0200

    Urls now call class-based views

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})', views.by_day),
-    url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})', views.by_month),
-    url(r'^(?P<year>[0-9]{4})', views.by_year),
-    url(r'^category/(?P<category_name>[A-Za-z_\-0-9]+)', views.category),
-    url(r'^(?P<article_id>[A-Za-z0-9]{6})', views.article),
-    url(r'^', views.homepage, name='homepage'),
+    url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})', views.SearchByDayView.as_view(), name='by_day'),
+    url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})', views.SearchByMonthView.as_view(), name='by_month'),
+    url(r'^(?P<year>[0-9]{4})', views.SearchByYearView.as_view(), name='by_year'),
+    url(r'^category/(?P<category_name>[A-Za-z_\-0-9]+)', views.CategoryView.as_view(), name='category'),
+    url(r'^(?P<article_id>[A-Za-z0-9]{6})', views.ArticleView.as_view(), name='article'),
+    url(r'^', views.HomepageView.as_view(), name='homepage'),
 
 ]
