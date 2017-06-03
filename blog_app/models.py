df7c1f45b9f16d05fdd79e433043686c3154b2a4
Mateusz Bednarski <echelon133@gmail.com>
2017-06-03 19:23:46 +0200

    Added more SiteDetails fields for authors name etc...

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,19 +1,34 @@
 import uuid
+import re
 from django.db import models
 
 def generate_id():
     uid = uuid.uuid4().hex
     return uid[:6]
 
-# Create your models here.
+def remove_not_safe(text):
+    expr = re.compile('[^a-zA-Z0-9 +\-]')
+    text = expr.sub('', text)
+    return text
+
+
 class SiteDetails(models.Model):
     headline = models.CharField(max_length=70)
-    description = models.TextField()
-    image = models.ImageField()
+    description = models.TextField(max_length=120)
+    author_nick = models.CharField(max_length=100)
+    author_description = models.TextField(max_length=300)
+    authors_twitter = models.URLField(blank=True)
+    authors_facebook = models.URLField(blank=True)
+    authors_github = models.URLField(blank=True)
+    authors_youtube = models.URLField(blank=True)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
+
+    def save(self, *args, **kwargs):
+        self.name = remove_not_safe(self.name.lower())
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -26,8 +41,6 @@ class Article(models.Model):
                                   max_length=6)
     title = models.CharField(max_length=100)
     last_modified = models.DateField(auto_now=True)
-    author = models.CharField(default='Admin',
-                              max_length=30)
     category = models.ManyToManyField(Category)
     article_body = models.TextField()
     slug = models.SlugField()
