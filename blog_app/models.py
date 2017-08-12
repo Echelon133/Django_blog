854df444d7c9a7a535e59b50cfc90d1962c5df3c
Mateusz Bednarski <echelon133@gmail.com>
2017-08-12 18:24:42 +0200

    Grant uniqueness of post hash

--- blog_app/models.py
+++ blog_app/models.py
@@ -38,7 +38,8 @@ class Article(models.Model):
     article_id = models.CharField(primary_key=True, 
                                   default=generate_id,
                                   editable=False,
-                                  max_length=6)
+                                  max_length=6
+                                  unique=True)
     title = models.CharField(max_length=100)
     last_modified = models.DateField(auto_now=True)
     category = models.ManyToManyField(Category)
