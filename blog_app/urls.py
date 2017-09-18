d4a2db71fb536b68762220eaf9d66e4f52f24478
Mateusz Bednarski <echelon133@gmail.com>
2017-09-18 14:56:44 +0200

    Now app doesn't allow random chars after / and throws 404

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^(?P<year>[0-9]{4})/?$', views.SearchByYearView.as_view(), name='by_year'),
     url(r'^category/(?P<category_name>[A-Za-z_ \-0-9+]+)', views.CategoryView.as_view(), name='category'),
     url(r'^(?P<article_id>[A-Za-z0-9]{6})/(?P<slug>[a-zA-z\-]+)', views.ArticleView.as_view(), name='article'),
-    url(r'^', views.HomepageView.as_view(), name='homepage'),
+    url(r'^$', views.HomepageView.as_view(), name='homepage'),
 ]
