54af8c21b4b6494addc8a65c3c17e25fbe668438
Mateusz Bednarski <echelon133@gmail.com>
2017-09-18 12:23:28 +0200

    Modified article url pattern. Now article urls can be generated using get_absolute_url

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/?$', views.SearchByMonthView.as_view(), name='by_month'),
     url(r'^(?P<year>[0-9]{4})/?$', views.SearchByYearView.as_view(), name='by_year'),
     url(r'^category/(?P<category_name>[A-Za-z_ \-0-9+]+)', views.CategoryView.as_view(), name='category'),
-    url(r'^(?P<article_id>[A-Za-z0-9]{6})', views.ArticleView.as_view(), name='article'),
+    url(r'^(?P<article_id>[A-Za-z0-9]{6})/(?P<slug>[a-zA-z\-]+)', views.ArticleView.as_view(), name='article'),
     url(r'^', views.HomepageView.as_view(), name='homepage'),
 ]
