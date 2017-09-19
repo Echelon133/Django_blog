1306eb9c66817067850cd7939a7818284704cb7f
Mateusz Bednarski <echelon133@gmail.com>
2017-09-19 15:57:18 +0200

    Added /login url

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^(?P<year>[0-9]{4})/?$', views.SearchByYearView.as_view(), name='by_year'),
     url(r'^category/(?P<category_name>[A-Za-z_ \-0-9+]+)', views.CategoryView.as_view(), name='category'),
     url(r'^(?P<article_id>[A-Za-z0-9]{6})/(?P<slug>[a-zA-z\-]+)', views.ArticleView.as_view(), name='article'),
-    url(r'^signup', views.SignupView.as_view(), name='signup'),
+    url(r'^signup$', views.SignupView.as_view(), name='signup'),
+    url(r'^login$', views.LoginView.as_view(), name='login'),
     url(r'^$', views.HomepageView.as_view(), name='homepage'),
 ]
