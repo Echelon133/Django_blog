5b953dc08993d8decb6b4a7f041c73415c3cfb64
Mateusz Bednarski <echelon133@gmail.com>
2017-09-21 21:01:32 +0200

    Added remaining tests for forms

--- blog_app/tests/test_forms.py
+++ blog_app/tests/test_forms.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from django.contrib.sessions.backends.db import SessionStore
 from django.test.client import RequestFactory
-from django import forms
 from django.http import QueryDict
 from blog_app.forms import UserLoginForm, UserSignupForm, CommentForm
 from blog_app.models import User, Comment, Article
+from django.core.exceptions import ValidationError
 
 
 class CommentFormTest(TestCase):
@@ -18,7 +18,7 @@ class CommentFormTest(TestCase):
         comment_form = CommentForm()
         author = User.objects.create_user('test', '', 'test_passwd1')
         article = Article.objects.create()
-        with self.assertRaises(forms.ValidationError):
+        with self.assertRaises(ValidationError):
             comment_form.save(author, article)
 
     def test_comment_form_works_properly_with_correct_data(self):
@@ -44,3 +44,54 @@ class LoginFormTest(TestCase):
         
         login_form = UserLoginForm(request, data=request.POST)
         self.assertTrue(login_form.is_valid())
+
+    def test_attempt_login_with_incorrect_data(self):
+        request_data = {'password': 'test',
+                        'username': 'testtest'}
+        request_querydict = QueryDict('', mutable=True)
+        request_querydict.update(request_data)
+
+        request = RequestFactory().post('/login', request_querydict)
+        request.session = SessionStore()
+
+        login_form = UserLoginForm(request, data=request.POST)
+        self.assertFalse(login_form.is_valid())
+        with self.assertRaises(ValidationError):
+            login_form.login()
+
+
+class UserSignupFormTest(TestCase):
+
+    def test_signup_new_user_with_correct_data(self):
+        new_user = {'username': 'new_username',
+                    'password1': 'my_new_password1',
+                    'password2': 'my_new_password1'}
+        
+        signup_form = UserSignupForm(data=new_user)
+        new_user_obj = signup_form.save()
+        self.assertEqual(new_user['username'], new_user_obj.username)
+
+    def test_signup_new_user_with_incorrect_data(self):
+        new_user = {'username': '',
+                    'password1': '',
+                    'password2': ''}
+        signup_form = UserSignupForm(data=new_user)
+        with self.assertRaises(ValueError):
+            signup_form.save()
+
+    def test_try_registering_user_with_existing_username(self):
+        new_user = {'username': 'new_username',
+                    'password1': 'my_new_password1',
+                    'password2': 'my_new_password1'}
+
+        User.objects.create_user(new_user['username'], '', new_user['password1'])
+        signup_form = UserSignupForm(data=new_user)
+        with self.assertRaises(ValueError):
+            signup_form.save()
+
+    def test_try_registering_user_with_incomplete_fields(self):
+        new_user = {'username': 'new_username', 'password1': 'my_new_password1'}
+        signup_form = UserSignupForm(data=new_user)
+        with self.assertRaises(ValueError):
+            signup_form.save()
+
