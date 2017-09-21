f151acddb53b1202ea748821f7acc648fac44a18
Mateusz Bednarski <echelon133@gmail.com>
2017-09-21 22:42:04 +0200

    Added test_models.py

---
+++ blog_app/tests/test_models.py
@@ -0,0 +1,70 @@
+from django.test import TestCase
+from blog_app.models import Category, Article, Comment
+from django.core.exceptions import ValidationError
+from django.utils.text import slugify
+
+
+class CategoryModelTest(TestCase):
+
+    def test_create_valid_category(self):
+        name = 'my-category-name'
+        category = Category(name=name)
+        category.save()
+
+        self.assertEqual(name, str(category))
+        url = '/category/' + name
+        self.assertEqual(url, category.get_absolute_url())
+
+    def test_try_creating_category_with_invalid_characters(self):
+        name = 'name!\n%^&(*#@#$)'
+        expected_name = 'name'
+        category = Category(name=name)
+        # When saving, category name is cleared from
+        # invalid characters. Only alphanum characters
+        # and - + are allowed
+        category.save()
+        category.full_clean()
+
+        self.assertEqual(str(category), expected_name)
+
+    def test_try_creating_empty_category(self):
+        category = Category()
+        with self.assertRaises(ValidationError):
+            category.full_clean()
+
+    def test_try_creating_category_with_long_name(self):
+        name = 'Test' * 100
+        category = Category(name=name)
+        with self.assertRaises(ValidationError):
+            category.full_clean()
+
+
+class ArticleModelTest(TestCase):
+    
+    def test_create_valid_article(self):
+        article = Article.objects.create()
+        category1 = Category.objects.create(name='test-category')
+        
+        article_title = 'My article title'
+        
+        test_article = {'article_id': 'FFFFFF',
+                        'title': article_title,
+                        'category': [category1, ],
+                        'article_body': 'text' * 200,
+                        'slug': slugify(article_title)}
+        
+        article.article_id = test_article['article_id'] 
+        article.title = test_article['title']
+        article.category = test_article['category']
+        article.article_body = test_article['article_body']
+        article.slug = test_article['slug']
+        article.save()
+
+        self.assertEqual(article.article_id, test_article['article_id'])
+        self.assertEqual(article.title, test_article['title'])
+        self.assertEqual(article.category.first(), test_article['category'][0])
+        self.assertEqual(article.article_body, test_article['article_body'])
+        self.assertEqual(article.slug, test_article['slug'])
+
+
+
