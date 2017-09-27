893a54186eee548a7df20ffc620dea256b47a318
Mateusz Bednarski <echelon133@gmail.com>
2017-09-27 23:10:25 +0200

    Now test cases for forms use CustomTestCase class

--- blog_app/tests/test_forms.py
+++ blog_app/tests/test_forms.py
@@ -5,9 +5,10 @@ from django.http import QueryDict
 from blog_app.forms import UserLoginForm, UserSignupForm, CommentForm
 from blog_app.models import User, Comment, Article
 from django.core.exceptions import ValidationError
+from .base import CustomTestCase
 
 
-class CommentFormTest(TestCase):
+class CommentFormTest(CustomTestCase):
     
     def test_comment_form_has_correct_restrictions(self):
         comment_form = CommentForm()
@@ -16,24 +17,24 @@ class CommentFormTest(TestCase):
 
     def test_comment_form_fails_with_incorrect_field_values(self):
         comment_form = CommentForm()
-        author = User.objects.create_user('test', '', 'test_passwd1')
-        article = Article.objects.create()
+        author = self.get_new_user(username='test', password='test_passwd1')
+        article = self.get_new_article()
         with self.assertRaises(ValidationError):
             comment_form.save(author, article)
 
     def test_comment_form_works_properly_with_correct_data(self):
         body = 'Test comment body'
         comment_form = CommentForm(data={'body': body})
-        author = User.objects.create_user('test1', '', 'testtest_pass123')
-        article = Article.objects.create()
+        author = self.get_new_user(username='test1', password='testtest_pass123')
+        article = self.get_new_article()
         comment_form.save(author, article)
         self.assertIn(body, comment_form.cleaned_data.values())
         
 
-class LoginFormTest(TestCase):
+class LoginFormTest(CustomTestCase):
 
     def test_login_with_correct_user(self):
-        user = User.objects.create_user('test_user', '', 'test_password3')
+        user = self.get_new_user(username='test_user', password='test_password3')
         request_data = {'password': 'test_password3',
                         'username': 'test_user'}
         request_querydict = QueryDict('', mutable=True)
@@ -60,7 +61,7 @@ class LoginFormTest(TestCase):
             login_form.login()
 
 
-class UserSignupFormTest(TestCase):
+class UserSignupFormTest(CustomTestCase):
 
     def test_signup_new_user_with_correct_data(self):
         new_user = {'username': 'new_username',
@@ -84,7 +85,7 @@ class UserSignupFormTest(TestCase):
                     'password1': 'my_new_password1',
                     'password2': 'my_new_password1'}
 
-        User.objects.create_user(new_user['username'], '', new_user['password1'])
+        self.get_new_user(username=new_user['username'], password=new_user['password1'])
         signup_form = UserSignupForm(data=new_user)
         with self.assertRaises(ValueError):
             signup_form.save()
