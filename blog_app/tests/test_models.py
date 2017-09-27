0e377071d4cdb0e5cc6a5c8a0af0a687a939fb44
Mateusz Bednarski <echelon133@gmail.com>
2017-09-27 23:35:59 +0200

    Now tests for models inherit from CustomTestCase

--- blog_app/tests/test_models.py
+++ blog_app/tests/test_models.py
@@ -2,13 +2,14 @@ from django.test import TestCase
 from blog_app.models import Category, Article, Comment, User
 from django.core.exceptions import ValidationError
 from django.utils.text import slugify
+from .base import CustomTestCase
 
 
-class CategoryModelTest(TestCase):
+class CategoryModelTest(CustomTestCase):
 
     def test_create_valid_category(self):
         name = 'my-category-name'
-        category = Category(name=name)
+        category = self.get_new_category(name=name)
         category.full_clean()
         category.save()
 
@@ -19,7 +20,7 @@ class CategoryModelTest(TestCase):
     def test_try_creating_category_with_invalid_characters(self):
         name = 'name!\n%^&(*#@#$)'
         expected_name = 'name'
-        category = Category(name=name)
+        category = self.get_new_category(name=name)
         # When saving, category name is cleared from
         # invalid characters. Only alphanum characters
         # and - + are allowed
@@ -29,47 +30,21 @@ class CategoryModelTest(TestCase):
         self.assertEqual(str(category), expected_name)
 
     def test_try_creating_empty_category(self):
-        category = Category()
+        category = self.get_new_category(name='')
         with self.assertRaises(ValidationError):
             category.full_clean()
 
     def test_try_creating_category_with_long_name(self):
         name = 'Test' * 100
-        category = Category(name=name)
+        category = self.get_new_category(name=name)
         with self.assertRaises(ValidationError):
             category.full_clean()
 
 
-class ArticleModelTest(TestCase):
-    
-    @staticmethod
-    def get_new_article(*, title=None, category=None, article_body=None):
-        if title is None:
-            art_title = ''
-            art_slug = ''
-        else:
-            art_title = title
-            art_slug = slugify(title)
-        
-        if category is None:
-            art_category = []
-        else:
-            art_category = category
-
-        if article_body is None:
-            art_body = ''
-        else:
-            art_body = article_body
-
-        new_article = Article.objects.create()
-        new_article.title = art_title
-        new_article.category = art_category
-        new_article.article_body = art_body
-        new_article.slug = art_slug
-        return new_article
+class ArticleModelTest(CustomTestCase):
     
     def test_create_valid_article(self):
-        category1 = Category.objects.create(name='test-category')
+        category1 = self.get_new_category(name='test-category')
         test_article = {'title': 'My article title',
                         'category': [category1, ],
                         'article_body': 'text' * 200}
@@ -89,7 +64,7 @@ class ArticleModelTest(TestCase):
         self.assertEqual(url, article.get_absolute_url())
 
     def test_try_creating_article_without_title(self):
-        category1 = Category.objects.create(name='test-category')
+        category1 = self.get_new_category(name='test-category')
         test_article = {'title': '',
                         'category': [category1, ],
                         'article_body': 'text' * 200}
@@ -102,7 +77,7 @@ class ArticleModelTest(TestCase):
             article.full_clean()
 
     def test_try_creating_article_without_body(self):
-        category1 = Category.objects.create(name='test-category')
+        category1 = self.get_new_category(name='test-category')
         test_article = {'title': 'My article title',
                         'category': [category1, ],
                         'article_body': ''}
@@ -115,32 +90,12 @@ class ArticleModelTest(TestCase):
             article.full_clean()
 
 
-class CommentModelTest(TestCase):
-    
-    @staticmethod
-    def get_new_comment(*, author=None, article_commented=None, body=None):
-        if author is None:
-            author = ''
-        else:
-            author = author
-        
-        if article_commented is None:
-            article_commented = ''
-        else:
-            article_commented = article_commented
-        
-        if body is None:
-            body = ''
-        else:
-            body = body
-
-        new_comment = Comment.objects.create(author=author, article_commented=article_commented, body=body)
-        return new_comment
+class CommentModelTest(CustomTestCase):
     
     def test_create_valid_comments(self):
-        author1 = User.objects.create_user('test_user', '', 'test_password1')
-        author2 = User.objects.create_user('test_user2', '', 'test_password123')
-        article1 = Article.objects.create()
+        author1 = self.get_new_user(username='test_user', password='test_password1')
+        author2 = self.get_new_user(username='test_user2', password='test_password123')
+        article1 = self.get_new_article()
 
         comment1 = self.get_new_comment(author=author1, article_commented=article1, body='Test')
         comment2 = self.get_new_comment(author=author2, article_commented=article1, body='Another text')
@@ -160,21 +115,21 @@ class CommentModelTest(TestCase):
         self.assertEqual(str(comment2), str_.format(comment2.author, comment2.body[:20]))
 
     def test_try_creating_comment_without_body(self):
-        author1 = User.objects.create_user('test_user', '', 'user_password_111')
-        article1 = Article.objects.create()
+        author1 = self.get_new_user(username='test_user', password='user_password_111')
+        article1 = self.get_new_article()
 
         comment = self.get_new_comment(author=author1, article_commented=article1)
         with self.assertRaises(ValidationError):
             comment.full_clean()
 
     def test_try_creating_comment_without_author(self):
-        article1 = Article.objects.create()
+        article1 = self.get_new_article()
 
         with self.assertRaises(ValueError):
             comment = self.get_new_comment(article_commented=article1, body='Test')
 
     def test_try_creating_comment_without_article(self):
-        author1 = User.objects.create_user('test_user', '', 'user_passwd123')
+        author1 = self.get_new_user(username='test_user', password='user_passwd123')
 
         with self.assertRaises(ValueError):
             comment = self.get_new_comment(author=author1, body='Test')
